557e230ee358b8c3b1ae9db9d355895ec3dcf5fb
zbl14 <ifthereisoneday@gmail.com>
2022-06-14 16:25:27 -0700

    creat form to collect name of player 1 and 2

--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,20 @@ PigDice.prototype.addScore = function() {
   }
 }
 
-let player1 = new PigDice("Ben");
 
+
+$(document).ready(function() {
+  attachContactListeners(); 
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+    const player1Name = $("input#player1Name").val();
+    const player2Name = $("input#player2Name").val();
+    const player1Score =;
+    const player2Score =;
+    let player1 = new PigDice(player1Name);
+    let player2 = new PigDice(player1Name);
+    player1.addScore();
+    player2.addScore();
+  });
+});
 
