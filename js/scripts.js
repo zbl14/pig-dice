9fa069c9ae6913afa72bc4a87025ee6f049f3e1f
zbl14 <ifthereisoneday@gmail.com>
2022-06-15 13:48:04 -0700

    WIP: refactory the switch function
    co-authored-by: Kyle Chinick <kylechinick@icloud.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
-function PigDice(name) {
+// Business Logics
+function PigDice(name, activePlayer) {
   this.name = name
   this.diceRoll = 0
   this.turnTotal = 0
   this.currentOverallScore = 0;
-  this.activePlayer = "active"; 
+  this.activePlayer = activePlayer; 
 }
 
 PigDice.prototype.roll = function() {
@@ -16,7 +17,8 @@ PigDice.prototype.roll = function() {
     } else {
       this.diceRoll = 1;
       this.turnTotal = 0;
-      this.activePlayer = "inactive";
+      //*** */ this.switch();
+      // this.activePlayer = "inactive";
       alert ("Your turn is over");
       return this.diceRoll;
     }
@@ -30,7 +32,8 @@ PigDice.prototype.hold = function () {
   console.log('this.currentOverallScore: ', this.currentOverallScore);
   this.turnTotal = 0;
   console.log('this.turnTotal: ', this.turnTotal);
-  this.activePlayer = "inactive";
+  //*** */ this.switch();
+  // this.activePlayer = "inactive";
   if (this.currentOverallScore >= 100) {
     alert ("Your win!");
   } 
@@ -44,37 +47,48 @@ PigDice.prototype.switch = function () {
   }
 }
 
-let player1 = new PigDice("Ben", 'active');
-let player2 = new PigDice("kyle", 'inactive');
-
-// function attachContactListeners() {
-//   $("#rollByOne").on("click", function() {
-//     player1.addScore();
-//     let player1Score = player1.currentScore
-//     console.log(player1Score);
-//   });
-//   $("#rollByTwo").on("click", function() {
-//     player2.addScore();
-//     console.log(player2.currentScore)
-//   });
-// }
-
+//UI Logics
 $(document).ready(function() {
-  // attachContactListeners(); 
   $("form#newPlayer").submit(function(event) {
     event.preventDefault();
-    const playerName = $("input#player1Name").val();
-
-    let player = new PigDice(playerName);
-
-    let player1Score = player1.currentScore
-
+    let player1Name = $("input#player1Name").val();
+    let player2Name = $("input#player2Name").val();
+    let player1 = new PigDice(player1Name, "active");
+    let player2 = new PigDice(player2Name, "inactive");
     $(".player1Name").html(player1.name);
-
-    $("#rollByOne").on("click", function() {
-      player1.addScore();
-      console.log(player1Score);
+    $(".player2Name").html(player2.name);
+    $(".player1TurnTotal").html(player1.turnTotal);
+    $(".player1OverallScore").html(player1.currentOverallScore);
+    $(".player2TurnTotal").html(player2.turnTotal);
+    $(".player2OverallScore").html(player2.currentOverallScore);
+ 
+    $('#rollByOne').click(function() { 
+      // if (player2.activePlayer === "inactive") {
+        // console.log(player1.activePlayer, player2.activePlayer )
+        // player1.switch();
+        // console.log(player1.activePlayer, player2.activePlayer )
+        player1.roll(); 
+        // console.log(player1.activePlayer, player2.activePlayer )
+        $(".player1TurnTotal").html(player1.turnTotal); 
+      // }
     });
+    $('#rollByTwo').click(function() {
+      // if (player1.activePlayer === "inactive") {
+        // player2.switch();
+        player2.roll(); 
+      $(".player2TurnTotal").html(player2.turnTotal); 
+      // } 
     });
-});
+    $('#holdByOne').click(function() { 
+      // console.log(player1.activePlayer, player2.activePlayer )
+      player1.hold();
+      // console.log(player1.activePlayer, player2.activePlayer )
+      $(".player1OverallScore").html(player1.currentOverallScore); 
+    });
+    $('#holdByTwo').click(function() { 
+      player2.hold();
+      $(".player2OverallScore").html(player2.currentOverallScore); 
+    });
+  });
+})
 
