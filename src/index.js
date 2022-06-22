412435675804bab9eb9af9deb0777a2365a93d4f
zbl14 <ifthereisoneday@gmail.com>
2022-06-22 10:21:58 -0700

    reorganize index.html index.js Pig-dice.js styles.css
    Co-authored-by: Peter Vang <vang_peter06@yahoo.com>

--- src/index.js
+++ src/index.js
@@ -1,61 +1,9 @@
-function PigDice(name, activePlayer) {
-  this.name = name;
-  this.diceRoll = 0;
-  this.turnTotal = 0;
-  this.currentOverallScore = 0;
-  this.activePlayer = activePlayer; 
-}
+import { PigDice } from './Pig-dice.js';
+import "bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import'./css/styles.css';
+import $ from 'jquery';
 
-let player1Instance = new PigDice("player1Name", "active");
-let player2Instance = new PigDice("player2Name", "inactive");
-let targetedScore = 10;
-
-PigDice.prototype.roll = function() {
-  if (this.activePlayer === "active") {
-    let score = Math.trunc(Math.random() * 6) + 1;
-    if (score !== 1) {
-      this.diceRoll = score;
-      console.log(this.diceRoll);
-      this.turnTotal += score;
-      // return this.diceRoll;
-    } else {
-      this.diceRoll = 1;
-      console.log(this.diceRoll);
-      this.turnTotal = 0;
-      this.switch();
-      disableArea ();
-      alert ("Your turn is over");
-      // return this.diceRoll;
-    }
-  } else {
-    alert ("It's not your turn yet!");
-  }
-}
-
-PigDice.prototype.hold = function () {
-  this.currentOverallScore += this.turnTotal;
-  this.turnTotal = 0;
-  this.switch();
-}
-
-PigDice.prototype.switch = function () {
-  let currentlyActivePlayer;
-  let currentlyWaitingPlayer;
-  if (player1Instance.activePlayer === "active") {
-    currentlyActivePlayer = player1Instance;
-    currentlyWaitingPlayer = player2Instance;
-  } else {
-    currentlyActivePlayer = player2Instance;
-    currentlyWaitingPlayer = player1Instance;
-  }
-  currentlyActivePlayer.activePlayer = "inactive";
-  currentlyWaitingPlayer.activePlayer = "active";
-console.log('currentlyActivePlayer: ', currentlyActivePlayer);
-console.log('currentlyWaitingPlayer: ', currentlyWaitingPlayer); 
-}
-
-
-//UI Logics
 function reset() {
   let thePlayers = [player1Instance, player2Instance];
   thePlayers.forEach(function(player) {
