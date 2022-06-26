5c8b66b4cf3ae10f05bf9b3ac60570acbf19b4d4
zbl14 <ifthereisoneday@gmail.com>
2022-06-26 14:11:50 -0700

    use class

--- src/js/pig-dice.js
+++ src/js/pig-dice.js
@@ -1,50 +1,29 @@
-export function PigDice(name) {
-  this.name = name;
-  this.diceRoll = 0;
-  this.turnTotal = 0;
-  this.currentOverallScore = 0;
-  this.activePlayer = 0; 
-}
-
-PigDice.prototype.roll = function() {
-  let randomNum = Math.trunc(Math.random() * 6) + 1;
-  if (randomNum !== 1) {
-    this.diceRoll = randomNum;
-    this.turnTotal += randomNum;
-  } else {
-    this.diceRoll = 1;
+export class PigDice {
+  constructor(name) {
+    this.name = name;
+    this.diceRoll = 0;
     this.turnTotal = 0;
+    this.currentOverallScore = 0;
+    this.activePlayer = 0;
   }
-};
-
-PigDice.prototype.hold = function () {
-  this.currentOverallScore += this.turnTotal;
-  this.diceRoll = 0;
-  this.turnTotal = 0;
-  this.activePlayer = 1;
-  return this.currentOverallScore
-};
-
-PigDice.prototype.switch = function() {
-  this.activePlayer = this.activePlayer === 0 ? 1 : 0;
-};
-
-// player1Instance
-// let player1Instance = new PigDice("player1Name", "active");
-// let player2Instance = new PigDice("player2Name", "inactive");
-
-// PigDice.prototype.switch = function () {
-//   let currentlyActivePlayer;
-//   let currentlyWaitingPlayer;
-//   if (player1Instance.activePlayer === "active") {
-//     currentlyActivePlayer = player1Instance;
-//     currentlyWaitingPlayer = player2Instance;
-//   } else {
-//     currentlyActivePlayer = player2Instance;
-//     currentlyWaitingPlayer = player1Instance;
-//   }
-//   currentlyActivePlayer.activePlayer = "inactive";
-//   currentlyWaitingPlayer.activePlayer = "active";
-//   console.log('currentlyActivePlayer: ', currentlyActivePlayer);
-//   console.log('currentlyWaitingPlayer: ', currentlyWaitingPlayer); 
-// };
+  roll() {
+    let randomNum = Math.trunc(Math.random() * 6) + 1;
+    if (randomNum !== 1) {
+      this.diceRoll = randomNum;
+      this.turnTotal += randomNum;
+    } else {
+      this.diceRoll = 1;
+      this.turnTotal = 0;
+    }
+  }
+  hold() {
+    this.currentOverallScore += this.turnTotal;
+    this.diceRoll = 0;
+    this.turnTotal = 0;
+    this.activePlayer = 1;
+    return this.currentOverallScore;
+  }
+  switch() {
+    this.activePlayer = this.activePlayer === 0 ? 1 : 0;
+  }
+}
