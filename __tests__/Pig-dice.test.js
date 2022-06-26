bf30a01b2fc12a29e041b5e3b66385b2028e4308
zbl14 <ifthereisoneday@gmail.com>
2022-06-26 14:14:08 -0700

    change import path for PigDice

--- __tests__/Pig-dice.test.js
+++ __tests__/Pig-dice.test.js
@@ -1,40 +1,38 @@
 import { PigDice } from './../src/Pig-dice.js';
 
 describe('PigDice', () => {
+  let player1, player2
+
+  beforeEach(() => {
+    player1 = new PigDice("A")
+    player2 = new PigDice("B")
+  });
 
   test('should return a object called player1, contains name,  diceRoll, turnTotal and currentOverallScore',() => {
-  let player1 = new PigDice ("player1");
-  expect(player1.name).toEqual("player1");
+  expect(player1.name).toEqual("A");
   expect(player1.diceRoll).toEqual(0);
   expect(player1.turnTotal).toEqual(0);
   expect(player1.currentOverallScore).toEqual(0);
   expect(player1.activePlayer).toEqual(0);  
   })
 
-  // test("should add scor to turnTotal if the score larger than 1", () => {
-  //   let randomNum = 2;
-  //   let player1 = new PigDice ("Player1", "active");
-  //   expect(player1.roll(randomNum)).toEqual(2);
-  // })
-
-  // test('should end turn if dice roll is a 1', () => {
-  //   let randomNum = 1;
-  //   let player1 = new PigDice("Player1", "active");
-  //   expect(player1.roll(randomNum)).toEqual("Your turn is over");
-  // });
-
-  test('should return 2-6 if not getting 1', () => {
-    let player1 = new PigDice("Player1");
+  test('should return 2 for both diceRoll and turnTotal, if roll a 2', () => {
     player1.roll();
-    if (player1.roll() !== 1) {
-      expect(player1.diceRoll).toBeGreaterThanOrEqual(2);
-      expect(player1.diceRoll).toBeLessThanOrEqual(6);
-    } else {
-      expect(player1.diceRoll).toEqual(1);
-      expect(player1.activePlayer).toEqual(1);
-    }   
+    if (player1.roll() === 2) {
+      expect(player1.diceRoll).toEqual(2);
+      expect(player1.turnTotal).toEqual(2);
+    }
   });
 
+  test('should return 1 for diceRoll, 0 for turnTotal, if roll a 1', () => {
+    player1.roll();
+    if (player1.roll() === 1) {
+      expect(player1.diceRoll).toEqual(1);
+      expect(player1.turnTotal).toEqual(0);
+    }
+  });
+
+  
   test("should add turnTotal score to currentOverallScore if press hold", () => {
     let player1 = new PigDice ("Player1");
     player1.hold();
@@ -46,19 +44,19 @@ describe('PigDice', () => {
 
   test("should make activePlayer to 0 for those getting 1", () => {
     let player1 = new PigDice ("Player1");
-    player1.enable()
+    player1.switch()
     if (player1.roll() === 1) {
-      player1.enable()
+      player1.switch()
       expect(player1.activePlayer).toEqual(0);
     }
   });
 
   test("should make activePlayer to 0 for those pressing hold ", () => {
     let player1 = new PigDice ("Player1");
-    player1.enable()
+    player1.switch()
     if (player1.roll() !== 1) {
       player1.hold()
-      player1.enable()
+      player1.switch()
       expect(player1.activePlayer).toEqual(0);
     }
   });
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { PigDice } from './Pig-dice.js';
+import { PigDice } from './../src/js/pig-dice.js';
 import "bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import'./css/styles.css';
